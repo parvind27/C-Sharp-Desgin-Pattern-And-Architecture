08a9b5effd8a2805da9c55bef4f7a85bad633cc8
Parvind Singh <parvind.s@thinkbridge.in>
2024-08-12 14:05:12 +0530

    Singleton Pattern In Asp.net

---
+++ SingletonDesginPattern/SingletonDesginPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingletonDesginPattern;
+
+Console.WriteLine("Hello, World!");
+
+var intance1 = SingeltonClass.Instance;
+var intance2 = SingeltonClass.Instance;
+if(intance1== intance2)
+{
+    Console.WriteLine("Same Instance");
+}
+Console.ReadLine();
---
+++ SingletonDesginPattern/SingletonDesginPattern/SingeltonClass.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesginPattern
+{
+    public class SingeltonClass
+    {
+        private static SingeltonClass? _instance;
+
+        public static SingeltonClass Instance
+        {
+
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SingeltonClass();
+                }
+                return _instance;
+
+            }
+
+        }
+
+        private SingeltonClass()
+        {
+
+        }
+    }
+
+    public class ThreadSafeSingeltonClass
+    {
+        private static readonly Lazy<ThreadSafeSingeltonClass>? _lazyInstance = new Lazy<ThreadSafeSingeltonClass>(()=>new ThreadSafeSingeltonClass());
+
+        public static ThreadSafeSingeltonClass Instance
+        {
+
+            get
+            {
+                 return _lazyInstance.Value;
+
+            }
+
+        }
+
+        private ThreadSafeSingeltonClass()
+        {
+
+        }
+    }
+
+}
